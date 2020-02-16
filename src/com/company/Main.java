da984d4bd3bbaf28d0f227f997908f1ab29e69f7
Горная <gornairyna@gmail.com>
2020-02-16 21:58:23 +0200

    Changed text for merge request conflict

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("new branch home");
-        System.out.println("Creeeted in IDEA");
+        System.out.println("Created new branch in IA");
     }
 }
