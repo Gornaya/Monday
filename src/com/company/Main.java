106303b585eac06cf4d5640cd5489e56ebfb0616
Горная <gornairyna@gmail.com>
2020-02-16 22:21:22 +0200

    Changed text

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("new branch home");
-        System.out.println("Created new bran A");
+        System.out.println("Created brunch");
     }
 }
